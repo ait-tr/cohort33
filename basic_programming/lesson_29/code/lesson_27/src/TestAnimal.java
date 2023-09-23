0dfe58f9098d8f2b7c0f40ca22a187e7c6f10d80
Zimmermann <igor.sonstnix@gmail.com>
2023-09-23 16:42:58 +0200

    lesson 29 was added

---
+++ basic_programming/lesson_29/code/lesson_27/src/TestAnimal.java
@@ -0,0 +1,15 @@
+package de.javalessons.homework25;
+
+public class TestAnimal {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.makeSound();
+        dog.breathe();
+
+
+        Animal.showCategory();
+
+
+    }
+}
